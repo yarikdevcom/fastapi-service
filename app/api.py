0fd51713df3864ab9201c8ba95a82f2a775369e2
Irishey <markirill21@gmail.com>
2022-02-14 14:53:10 +0200

    add bots' endpoints

--- app/api.py
+++ app/api.py
@@ -4,7 +4,7 @@ from dependency_injector.wiring import inject, Provide
 from app.services import ModelQueryService
 
 from .containers import AppContainer
-from .models import Content, ContentIn
+from .models import Content, ContentIn, Bot, BotIn
 
 from .tasks import fetch_content
 
@@ -17,6 +17,26 @@ async def get_content_many(
     query=Depends(Provide[AppContainer.content.query]),
 ):
     return await query.all()
+
+
+@router.get("/bots", response_model=list[Bot])
+@inject
+async def get_bot_many(
+    query=Depends(Provide[AppContainer.bot.query])
+):
+    return await query.all()
+
+
+@router.post("/bot", response_model=Bot)
+@inject
+async def create_bot(
+    bot_in: BotIn,
+    query: ModelQueryService = Depends(Provide[AppContainer.bot.query])
+):
+    bot: Bot = await query.create(bot_in)
+    await query.commit()
+    fetch_content.delay(bot.id)
+    return bot
 
 
 @router.post("/contents", response_model=Content)
