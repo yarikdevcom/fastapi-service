e81b8d95710d8596c60248392dee182a22a0df55
Morty Space <morty.space@gmail.com>
2022-02-20 16:20:55 +0300

    Updated simple test to check cache

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -6,3 +6,6 @@ async def test_simple_redis(container):
     redis = container.resources.redis()
     await redis.set("123", 100_000)
     assert int(await redis.get("123")) == 100_000
+
+    await redis.set("123", "asdasd")
+    assert (await redis.get("123")).decode('utf-8') == "asdasd"
